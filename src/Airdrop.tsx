d53013fb471e74217e56b0f8349f6b976494929c
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-18 22:22:18 +0545

    text msg added

--- src/Airdrop.tsx
+++ src/Airdrop.tsx
@@ -1,18 +1,24 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useRef } from "react";
 
 
 export function Airdrop(){
+    const amountRef = useRef<HTMLInputElement>(null);
 const wallet = useWallet();
 const {connection} = useConnection();
 function sendAirdropToUser(){
     if(!wallet.publicKey) return;
-    //@ts-ignore
-    const amount = document.getElementById("publicKey").value
+    const amount = Number(amountRef.current?.value || 0);
+    if (amount === undefined) {
+  console.error("Amount input not found.");
+}
+
     connection.requestAirdrop(wallet.publicKey, amount *1000000000)
 }
     return (
-        <div>
-            <input id="publickey" type="text" placeholder="Amount"/>
+        <div className="space-y-8">
+            <h1>welcome to solana faucat</h1>
+            <input ref={amountRef} id="publickey" type="text" placeholder="Amount"/>
             <button onClick={sendAirdropToUser}>Send Airdrop</button>
         </div>
     )
